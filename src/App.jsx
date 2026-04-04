ca1521145406873f29d64d8683bc0d5e6915c2b3
shinpagayanan <pagayananshane@gmail.com>
2026-04-04 19:54:39 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import './App.css';
+import { Navbar } from './Components/Navbar/Navbar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import { Shop } from './Pages/Shop';
+import { ShopCategory } from './Pages/ShopCategory';
+import { LoginSignup } from './Pages/LoginSignup';
+import { Product } from './Pages/Product';
+import { Cart } from './Pages/Cart';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+
+      <Routes>
+        <Route path='/' element={<Shop />} />
+        <Route path='/mens' element={<ShopCategory category="men" />} />
+        <Route path='/womens' element={<ShopCategory category="women" />} />
+        <Route path='/kids' element={<ShopCategory category="kid" />} />
+
+        <Route path='/product' element={<Product />}>
+          <Route path=':productId' element={<Product />} />
+        </Route>
+
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/login' element={<LoginSignup />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
